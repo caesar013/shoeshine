8389e659c319ae3dd06742b44bc56a81538562ed
Fikril H Ramadhani <fikril099@gmail.com>
2023-06-09 21:30:23 +0700

    addressing routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ShoeController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +18,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/logout', function(){
+    abort(403);
+});
+Route::middleware('auth')->group(function(){
+    Route::middleware('isAdmin')->group(function(){
+
+    });
+});
+Route::get('/', function(){
+    return redirect()->route('login')->with('title', 'Login');
+});
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::resource('/order', OrderController::class);
+
+Route::resource('/service', ServiceController::class);
+
+Route::resource('/shoe', ShoeController::class);
+
+Route::resource('/transaction', TransactionController::class);
